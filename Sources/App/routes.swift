5c1d7aa9c8fda4912c7cf7da68366ab7cbabf731
Linus S <LinusS1@users.noreply.github.com>
2020-10-03 12:16:27 -0700

    Add Slack Events api verification, and slack verification.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,12 +2,15 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
+    app.post("slack", "events") { req -> String in
+        let contentType = try req.content.decode(SlackEventType.self)
+        guard contentType.token == Environment.get("VERIFICATION_TOKEN") else { throw Abort(.imATeapot) }
+        if contentType.type == "url_verification" {
+            let content = try req.content.decode(SlackEventVerification.self)
+            return content.challenge
+        } else {
+            return ""
+        }
     }
-
 }
