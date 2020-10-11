7395cc6f0e0689294c39feb2796a4fb7850ccdac
Linus S <LinusS1@users.noreply.github.com>
2020-10-10 20:05:05 -0700

    Move to Swift 5.3 (#21)
    * Move to Swift 5.3
    
    * Delete Docker Stuff
    
    * Added Heroku Database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,19 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "linus",
-        password: Environment.get("DATABASE_PASSWORD") ?? "",
-        database: Environment.get("DATABASE_NAME") ?? "rudolph"
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .forClient(certificateVerification: .none)
+        app.databases.use(.postgres(
+            configuration: postgresConfig
+        ), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            username: Environment.get("DATABASE_USERNAME") ?? "rudolph",
+            password: Environment.get("DATABASE_PASSWORD") ?? "",
+            database: Environment.get("DATABASE_NAME") ?? "rudolph"
+        ), as: .psql)
+    }
 
     app.migrations.add(CreateSubmittedPullRequest())
 
